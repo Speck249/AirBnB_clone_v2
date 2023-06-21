44a81d832a659f87e02081c22274fe7edf0fd13b
Rediet <redietkfle@gmail.com>
2023-06-21 10:02:29 -0700

    Merge conflict resolve

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,10 +43,10 @@ class FileStorage:
     def reload(self):
         """Loads storage dictionary from file"""
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+                "BaseModel": BaseModel, "User": User, "State": State,
+                "Place": Place, "City": City, "Amenity": Amenity,
+                "Review": Review
+                }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -56,7 +56,6 @@ class FileStorage:
                     eval(obj)(**val)
         except FileNotFoundError:
             pass
-
 
     def delete(self, obj=None):
         """
