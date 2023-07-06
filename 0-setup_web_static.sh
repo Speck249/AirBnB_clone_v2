95d1e4114bd88aed1e07673807d39acdc88e579c
Rediet <redietkfle@gmail.com>
2023-07-06 10:37:33 -0700

    Set up webservers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,15 +12,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 
 # Create fake HTML file
-echo "<html>
-  <head>
-    <title>
-    </title>
-  </head>
-  <body>
-        Test Content
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+echo "Test Content" | sudo tee /data/web_static/releases/test/index.html
 
 # Create symbolic link 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -29,7 +21,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i '/^server {/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 # Restart nginx
 sudo service nginx restart
